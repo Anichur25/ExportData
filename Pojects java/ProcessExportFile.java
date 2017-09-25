5a811175e2d66f98efd57529e9a3b05016e0e65c
anis25 <aniscseru6125@gmail.com>
2017-09-25 23:42:53 +0600

    Finally my work successfully done...

--- Pojects java/ProcessExportFile.java
+++ Pojects java/ProcessExportFile.java
@@ -36,16 +36,50 @@ public class ProcessExportFile {
 		}
 	}
 
+	public int numberOfExporters(CSVParser parser, String string) 
+	{
+		int exporters = 0;
+		for(CSVRecord record : parser)
+		{
+			if(record.get("Exports").contains(string))
+			{
+				exporters++;
+			}
+		}
+		return exporters;
+	}
+
+	public void bigExporters(CSVParser parser, String string)
+	{
+		String x = string.replace("$", "");
+		x = x.replaceAll(",", "");
+		long i = Long.parseLong(x);
+
+		for(CSVRecord record : parser)
+		{
+			String a = record.get("Value (dollars)");
+			String b = a.replace("$", "");
+			b = b.replaceAll(",", "");
+			long j = Long.parseLong(b);
+			if(j > i)
+			{
+				System.out.println(record.get("Country") + " " + record.get("Value (dollars)"));
+			}
+		}
+
+	}
+
 
 	public void tester()
 	{
 		FileResource fr = new FileResource();
 		CSVParser parser = fr.getCSVParser();
+		System.out.println("Number of exporters : " + numberOfExporters(parser,"gold"));
 
 		String sp = countryInfo(parser,"Malaysia");
 		listExportersTwoProducts(parser,"gold","diamonds");
-
 		System.out.println(sp);
+		bigExporters(parser, "$999,999,999");
 	}
 
 	public static void main(String[] args) {
