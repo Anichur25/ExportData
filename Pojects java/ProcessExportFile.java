33b2eb3b299e6b4dbc1f82e678b1a22bf7930b07
anis25 <aniscseru6125@gmail.com>
2017-09-25 23:05:47 +0600

    There is a only one method called 'tester()' which get the file name and parse it on CSVParser...

---
+++ Pojects java/ProcessExportFile.java
@@ -0,0 +1,59 @@
+import org.apache.commons.csv.*;
+import edu.duke.*;
+
+public class ProcessExportFile {
+
+
+	/*public String countryInfo(CSVParser parser,String country)
+	{
+		String result = "NOT FOUND";
+
+		for(CSVRecord rec : parser)
+		{
+			if(rec.get(0).equals(country))
+			{
+				result = rec.get(0) + " : " + rec.get(1) + " : " + rec.get(2);
+				break;
+			}
+
+
+		}
+
+
+		return result;
+	}*/
+
+
+	/*public void listExportersTwoProducts(CSVParser parser,String exportItem1,String exportItem2)
+	{
+
+		for(CSVRecord rec : parser)
+		{
+			if(rec.get(1).contains(exportItem1) && rec.get(1).contains(exportItem2))
+			{
+				System.out.println(rec.get(0));
+			}
+		}
+	}*/
+
+
+	public void tester()
+	{
+		FileResource fr = new FileResource();
+		CSVParser parser = fr.getCSVParser();
+
+		//String sp = countryInfo(parser,"Malaysia");
+		//listExportersTwoProducts(parser,"gold","diamonds");
+
+		//System.out.println(sp);
+	}
+
+	/*public static void main(String[] args) {
+
+		ProcessExportFile test = new ProcessExportFile();
+
+		test.tester();
+
+	}*/
+
+}
